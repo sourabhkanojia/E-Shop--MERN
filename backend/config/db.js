36c7b5852a6540bfd9a1acf385c65167bc3010ae
Sourabh Kanojia <sunnycarlos2303@gmail.com>
2021-08-29 00:40:46 +0530

    modelling database

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,13 @@ const connectDB = async () => {
         const conn = await mongoose.connect(process.env.MONGO_URI,{
             useUnifiedTopology: true,
             useNewUrlParser: true,
-            useCreateIndex: true
         })
+
+        console.log(`MongoDB Connected: ${conn.connection.host}`)
+    } catch (error) {
+        console.log(`Error: ${error.message}`)
+        process.exit(1)
     }
 }
+
+export default connectDB
