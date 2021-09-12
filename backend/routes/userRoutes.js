0aef8b61cd269592d63792df57fc7f6a5a0feead
Sourabh Kanojia <sunnycarlos2303@gmail.com>
2021-09-12 15:10:22 +0530

    added middleware for users

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,9 @@
 import express from 'express';
 const router = express.Router();
-import { authUser, } from '../controllers/userController.js'
+import { authUser, gethUserProfile } from '../controllers/userController.js'
+import { protect } from '../middleware/authMiddleware.js';
 
 router.post('/login', authUser);
+router.route('/profile').get(protect, gethUserProfile);
 
 export default router;
