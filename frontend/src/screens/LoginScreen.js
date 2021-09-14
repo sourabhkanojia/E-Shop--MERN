b6b10d841a0cdedbe3a6b29da7d1a14db0583c5f
Sourabh Kanojia <sunnycarlos2303@gmail.com>
2021-09-15 00:41:02 +0530

    login and logout set

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -51,7 +51,7 @@ const LoginScreen = ({ location, history }) => {
           <Form.Control
             type="password"
             placeholder="Enter password"
-            value={email}
+            value={password}
             onChange={(e) => setPassword(e.target.value)}
           ></Form.Control>
         </Form.Group>
