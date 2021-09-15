70326344db6aa288e245994b9abd7c786ba36435
Sourabh Kanojia <sunnycarlos2303@gmail.com>
2021-09-15 15:10:00 +0530

    profile screen crated

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,18 @@
-import express from 'express';
+import express from "express";
 const router = express.Router();
-import { authUser, registerUser, gethUserProfile } from '../controllers/userController.js'
-import { protect } from '../middleware/authMiddleware.js';
+import {
+  authUser,
+  registerUser,
+  gethUserProfile,
+  updatehUserProfile,
+} from "../controllers/userController.js";
+import { protect } from "../middleware/authMiddleware.js";
 
-router.route('/').post(registerUser);
-router.post('/login', authUser);
-router.route('/profile').get(protect, gethUserProfile);
+router.route("/").post(registerUser);
+router.post("/login", authUser);
+router
+  .route("/profile")
+  .get(protect, gethUserProfile)
+  .put(protect, updatehUserProfile);
 
 export default router;
